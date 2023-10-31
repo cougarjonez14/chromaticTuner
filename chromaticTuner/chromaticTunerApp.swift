9fe04c92be954aa104f0c77b17d474873b40d6ca
stationAstral <cougarjonez@gmail.com>
2023-10-31 09:24:35 -0700

    Initial Commit

---
+++ chromaticTuner/chromaticTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  chromaticTunerApp.swift
+//  chromaticTuner
+//
+//  Created by stationAstral on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct chromaticTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
