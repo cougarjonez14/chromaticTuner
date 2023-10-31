3d4d082547c2d63e3773cae2cfa0680d9b3d8027
stationAstral <cougarjonez@gmail.com>
2023-10-31 09:31:52 -0700

    1.0

--- chromaticTuner/ContentView.swift
+++ chromaticTuner/ContentView.swift
@@ -7,18 +7,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ChromaticTunerView: View {
+
+    @State private var noteName = "A"
+    @State private var noteFrequency = 440.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(noteName)
+                .font(.largeTitle)
+
+            Text("\(noteFrequency, specifier: "%.2f") Hz")
+                .font(.title)
+
+            Button(action: {
+                // Start the tuner
+            }) {
+                Text("Start")
+            }
+Spacer()
+            // Add a note indicator of a full octave
+            Image(systemName: "pianokeys")
+                .resizable()
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 100)
+    
+                            // Add a needle
+            Image(systemName: "arrow.up.and.down")
+                .resizable()
+                .frame(width: 50, height: 100)
+                .padding(100)
+            // Add a flat/sharp indicator
+            Image(systemName: "music.quarternote.32")
+                .resizable()
+            //change color to red
+                .foregroundColor(.red)
+                .frame(width: 30, height: 30)
+                .position(x: 150, y: 400)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ChromaticTunerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChromaticTunerView()
+    }
 }
+
--- chromaticTuner/chromaticTunerApp.swift
+++ chromaticTuner/chromaticTunerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct chromaticTunerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ChromaticTunerView()
         }
     }
 }
